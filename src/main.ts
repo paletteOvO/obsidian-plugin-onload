ab268a2ddaf7b131cf3ad878ba4b62f3b74958af
paletteOvO <paletteOvO@outlook.com>
2021-12-01 02:54:02 +0800

    onload: init commit and poc

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,27 @@
 import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
 
 export default class MyPlugin extends Plugin {
-  // This field stores your plugin settings.
-  setting: MyPluginSettings;
+   // This field stores your plugin settings.
+   setting: MyPluginSettings;
 
-  onInit() {}
+   onInit() {}
 
-  async onload() {
-    console.log("Plugin is Loading...");
+   async onload() {
+      console.log("Plugin is Loading...");
 
-    // This snippet of code is used to load pluging settings from disk (if any)
-    // and then add the setting tab in the Obsidian Settings panel.
-    // If your plugin does not use settings, you can delete these two lines.
-    this.setting = (await this.loadData()) || {
-      someConfigData: 1,
-      anotherConfigData: "defaultValue",
-    };
-    this.addSettingTab(new MyPluginSettingsTab(this.app, this));
-  }
+      // This snippet of code is used to load pluging settings from disk (if any)
+      // and then add the setting tab in the Obsidian Settings panel.
+      // If your plugin does not use settings, you can delete these two lines.
+      this.setting = (await this.loadData()) || {
+         onload: "",
+      };
+      Function(this.setting.onload).bind(window)();
+      this.addSettingTab(new MyPluginSettingsTab(this.app, this));
+   }
 
-  onunload() {
-    console.log("Plugin is Unloading...");
-  }
+   onunload() {
+      console.log("Plugin is Unloading...");
+   }
 }
 
 /**
@@ -29,32 +29,28 @@ export default class MyPlugin extends Plugin {
  * as you wish by adding fields and all the data you need.
  */
 interface MyPluginSettings {
-  someConfigData: number;
-  anotherConfigData: string;
+   onload: string;
 }
 
 class MyPluginSettingsTab extends PluginSettingTab {
-  plugin: MyPlugin;
+   plugin: MyPlugin;
 
-  constructor(app: App, plugin: MyPlugin) {
-    super(app, plugin);
-    this.plugin = plugin;
-  }
+   constructor(app: App, plugin: MyPlugin) {
+      super(app, plugin);
+      this.plugin = plugin;
+      const { containerEl } = this;
+      const settings = this.plugin.setting;
 
-  display(): void {
-    const { containerEl } = this;
-    const settings = this.plugin.setting;
-    // This is just an example of a setting controll.
-    new Setting(containerEl)
-      .setName("Setting Name")
-      .setDesc("Setting description")
-      .addText((text) =>
-        text.setValue(String(settings.someConfigData)).onChange((value) => {
-          if (!isNaN(Number(value))) {
-            settings.someConfigData = Number(value);
-            this.plugin.saveData(settings);
-          }
-        })
-      );
-  }
+      new Setting(containerEl)
+         .setName("Onload")
+         .setDesc("")
+         .addText((text) =>
+            text.setValue(String(settings.onload)).onChange((value) => {
+               settings.onload = value;
+               this.plugin.saveData(settings);
+            }),
+         );
+   }
+
+   display(): void {}
 }
